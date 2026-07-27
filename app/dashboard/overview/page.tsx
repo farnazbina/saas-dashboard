00eeff9e871f22f0ff16bee00f1d6cc17d6afe41
farnazbina <farnaz.bina99@gmail.com>
2026-07-27 16:32:59 +0330

    add task distribution chart

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -1,15 +1,18 @@
 import OverviewSection from "@/components/dashboard/OverviewSection";
 import { ProjectProgressSection } from "@/components/dashboard/ProjectProgressSection";
+import { TaskDistributionSection } from "@/components/dashboard/TaskDistributionSection";
 
 export default function DashboardPage() {
     return (
-        <div className="flex flex-col items-center justify-center gap-4 w-full">
+        <div className="flex flex-col items-center justify-center gap-6 w-full">
             <OverviewSection />
             <div className="w-full grid grid-cols-6 gap-6">
                 <div className="col-span-4">
                     <ProjectProgressSection />
                 </div>
-                <div className="col-span-2"></div>
+                <div className="col-span-2">
+                    <TaskDistributionSection />
+                </div>
             </div>
         </div>
     )
---
+++ components/dashboard/TaskDistributionSection.tsx
@@ -0,0 +1,120 @@
+"use client"
+
+import * as React from "react"
+import { TrendingUp } from "lucide-react"
+import { Label, Pie, PieChart } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+    type ChartConfig,
+} from "@/components/ui/chart"
+
+export const description = "A donut chart with text"
+
+const chartData = [
+    { browser: "chrome", visitors: 275, fill: "#7a13f0" },
+    { browser: "safari", visitors: 200, fill: "#aa7dff" },
+    { browser: "firefox", visitors: 287, fill: "#19724f" },
+    { browser: "edge", visitors: 173, fill: "#155dfc" },
+    { browser: "other", visitors: 190, fill: "#ff6b6b" },
+]
+
+const chartConfig = {
+    visitors: {
+        label: "Visitors",
+    },
+    chrome: {
+        label: "Chrome",
+        color: "var(--chart-1)",
+    },
+    safari: {
+        label: "Safari",
+        color: "var(--chart-2)",
+    },
+    firefox: {
+        label: "Firefox",
+        color: "var(--chart-3)",
+    },
+    edge: {
+        label: "Edge",
+        color: "var(--chart-4)",
+    },
+    other: {
+        label: "Other",
+        color: "var(--chart-5)",
+    },
+} satisfies ChartConfig
+
+export function TaskDistributionSection() {
+    const totalVisitors = React.useMemo(() => {
+        return chartData.reduce((acc, curr) => acc + curr.visitors, 0)
+    }, [])
+
+    return (
+        <Card className="flex flex-col h-90 rounded-lg">
+            <CardHeader className="items-center pb-0">
+                <CardTitle>Task Distribution</CardTitle>
+            </CardHeader>
+            <CardContent className="flex-1 pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square h-85 -mt-8"
+                >
+                    <PieChart>
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Pie
+                            data={chartData}
+                            dataKey="visitors"
+                            nameKey="browser"
+                            innerRadius={90}
+                            strokeWidth={4}
+                        >
+                            <Label
+                                content={({ viewBox }) => {
+                                    if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                        return (
+                                            <text
+                                                x={viewBox.cx}
+                                                y={viewBox.cy}
+                                                textAnchor="middle"
+                                                dominantBaseline="middle"
+                                            >
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={viewBox.cy}
+                                                    className="fill-foreground text-3xl font-bold"
+                                                >
+                                                    {totalVisitors.toLocaleString()}
+                                                </tspan>
+                                                <tspan
+                                                    x={viewBox.cx}
+                                                    y={(viewBox.cy || 0) + 24}
+                                                    className="fill-muted-foreground"
+                                                >
+                                                    Visitors
+                                                </tspan>
+                                            </text>
+                                        )
+                                    }
+                                }}
+                            />
+                        </Pie>
+                    </PieChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
